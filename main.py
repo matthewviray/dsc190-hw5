77d271fddeac4c68ca8ee4a239bd21f1468621f9
Matthew Viray <matvir437@gmail.com>
2026-04-30 22:32:08 -0700

    fixing things?

--- main.py
+++ main.py
@@ -1,25 +1,9 @@
 def main():
-    x=1
-    y = 2
-    z = x+y
-    print( "Hello from dsc190-hw5!" )
-    l = [1,2,3]
-
-if __name__ == "__main__":
-    main()import os
-import sys
-
-def main():
-    x=1
-    y = 2
-    z = x+y
+    result = 1 + 2
+    my_list = [1, 2, 3]
     print("Hello from dsc190-hw5!")
-    print("unused variable: " + str(z))
-
-if __name__ == "__main__":
-    main()def main():
-    print("Hello from dsc190-hw5!")
-
+    print(result)
+    print(my_list)
 
 if __name__ == "__main__":
     main()
